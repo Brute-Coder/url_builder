455ae71dcf4e3befd9be727a46208e6781b22404
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-07-18 05:35:28 +0530

    initial and most final commit

---
+++ components/data/masterData.ts
@@ -0,0 +1,123 @@
+const Categories = ["tradewise", "astrolearn"];
+const PageTypes = ["paid", "free"];
+const astroTeachers = [
+  "krish",
+  "rohet",
+  "pravin",
+  "vivek",
+  "jasmine",
+  "aastha",
+  "vaibhav",
+  "saurav",
+];
+const tradewiseTeachers = [
+  "abhishek",
+  "ashutosh",
+  "asseem",
+  "bala",
+  "bhika",
+  "chirag",
+  "hemant",
+  "jagpreet",
+  "jasmin",
+  "jawaad",
+  "kamalpreet",
+  "munish",
+  "nilesh",
+  "nirlep",
+  "pankaj",
+  "prabhu",
+  "rajesh",
+  "raju",
+  "vikram",
+];
+
+const tradewise_short_free_slugs = [
+  "abhishek-options",
+  "asseem-algo",
+  "bala-market",
+  "hemant-swing",
+  "abhishek-system",
+  "jagpreet-intraday",
+  "jawaad-forex",
+  "kamalpreet-swing",
+  "munish-crypto",
+  "prabhu-swing",
+  "rajesh-algo",
+  "raju-options",
+  "vikram-algo",
+];
+
+const tradewise_long_free_slugs = [
+  "abhishek-options",
+  "ashutosh-elliot",
+  "asseem-algo",
+  "bala-market",
+  "bhika-options",
+  "chirag-options",
+  "hemant-swing",
+  "abhishek-system",
+  "jagpreet-intraday",
+  "jasmin-trading",
+  "jawaad-forex",
+  "kamalpreet-swing",
+  "munish-crypto",
+  "nilesh-price-action",
+  "nirlep-forex",
+  "pankaj-scalping",
+  "prabhu-swing",
+  "rajesh-algo",
+  "raju-options",
+  "vikram-algo",
+];
+const astro_short_free_slugs = [
+  "krish-kp",
+  "rohet-crystal",
+  "pravin-vedic",
+  "jasmine-tarot",
+  "aastha-switch",
+  "vaibhav-bnn",
+  "saurav-bnn",
+];
+
+const astro_long_free_slugs = [
+  "krish-kp",
+  "vaibhav-bnn",
+  "pravin-vedic",
+  "rohet-crystal",
+  "saurav-bnn",
+];
+
+const tradewise_paid_video_slug = [
+  "abhishek-options",
+  "jawaad-forex",
+  "chirag-options",
+  "prabhu-swing",
+  "jagpreet-intraday",
+  "munish-crypto",
+  "bala-market",
+  "pankaj-scalping",
+];
+
+const tradewise_free_page_formats = ["short", "long"];
+const astro_free_page_formats = ["short", "long"];
+const tradewise_paid_page_formats = ["video"];
+const astro_paid_page_formats: Array<string> = [];
+const source = ["facebook", "google"];
+
+export {
+  Categories,
+  PageTypes,
+  astroTeachers,
+  tradewiseTeachers,
+  tradewise_short_free_slugs,
+  tradewise_long_free_slugs,
+  astro_short_free_slugs,
+  astro_long_free_slugs,
+  tradewise_paid_video_slug,
+  tradewise_free_page_formats,
+  astro_free_page_formats,
+  tradewise_paid_page_formats,
+  astro_paid_page_formats,
+  source,
+};
